eb926b35355746a498f218a22697ed3fc3fb1555
Valentin <gimenezvalentin99@gmail.com>
2025-12-09 19:53:49 -0300

    Get Municipios funcionando

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import json
 from pathlib import Path
 
 json_path = Path(__file__).parent / "municipios.json"
 
 def cargar_datos():
-    with open(json_path, 'r', encoding='utf-8') as file:
-        datos = json.load(file)
-    return datos
+    if not json_path.exists():
+        return {"municipios": []}
+    
+    with open(json_path, 'r', encoding='utf-8') as file:  
+        return json.load(file)
 
+def guardar_datos(datos):
+    with open(json_path, 'w', encoding='utf-8') as file:
+        json.dump(datos, file, indent=4, ensure_ascii=False)
+        
 app = FastAPI()
-
+security = HTTPBasic()
 
 #aca iria toda la data gral de la api
 @app.get("/")
@@ -21,7 +28,7 @@ def read_root():
 @app.get("/municipios")
 def obtener_municipios():
     datos = cargar_datos()
-    return datos
+    return datos.get("municipios", [])
 
 #buscar un municipio por su id
 @app.get("/municipios/{id_municipio}")
@@ -32,3 +39,5 @@ def obtener_municipio(id_municipio: str):
     for municipio in municipios:
         if municipio.get("id") == id_municipio:
             return municipio
+    
+    raise HTTPException(status_code=404, detail="Municipio no encontrado")
